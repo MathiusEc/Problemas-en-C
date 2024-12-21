83aed4423d241373b73112343d78e48018eac2c4
Isaac Benítez <francisco.malquin@outlook.es>
2024-12-20 21:45:38 -0500

    ejercicio matusalen by Isa4cc

---
+++ Ejercicio-1/EJERCICIO1.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+/****** 
+Realizar un programa en C que permita acertar la edad de Matusalen(969)
+el programa debe indicar si es la respuesta correcta, caso contrario indicar si es menor o mayor.
+
+Se debe permitir solo 3 intentos, si acierta dentro de los 3 intentos debe terminar el programa.
+
+Al final del programa si no acertó se debe desplegar un mensaje de que lo intente nuevamente.
+Si dice si, se debe permitir repetir el programa.
+******/
+//Made by ñeñitez
+
+int main(){
+
+    int respuesta, intentos;
+    char opci;
+    
+    do{
+
+        do{
+            printf("\nDear user, please enter Methuselah's age, remember you only have 3 attempts..\n");
+            scanf("%d", &respuesta);
+            if(respuesta<0){
+                printf("\nPlease, enter a number in a valid range\n");
+            }
+        }while(respuesta<0);
+
+        intentos=0;
+        if(respuesta==969){
+            printf("\nCongrats!! You won. 969 is the correct answer1");
+        }else{
+            intentos=0;
+            do{
+                printf("Incorect! Try again..\n");
+                scanf("%d", &respuesta);
+                intentos++;
+            }while(respuesta!=969);
+            if(intentos<=3){
+                printf("\nCongrats man!! Your answer is ok :}\n");
+            }else{
+                printf("Sorry, you lose because you did not answer in 3 attemps or less.. :c\n");
+            }
+         }
+
+         do{
+            printf("\nDo you want to play again? Y/N\n");
+            scanf(" %c", &opci);/*por favor, recuerden que después de una lectura de int, al momento de pasar a char
+            se debe de dar un espacio a la máscara de la variable en scanf. cuestiones técnicas jaja*/
+        }while(opci!='N' && opci!='n'  && opci!='y' && opci!='Y');
+
+    }while(opci=='y' || opci=='Y');
+        
+    return 0;
+}
